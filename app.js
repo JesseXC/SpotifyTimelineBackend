3ffdac8e84529b139e2b884bbec8397ef1f16038
JesseXC <jcruz2@oberlin.edu>
2024-01-07 20:16:18 -0800

    Fixed typo

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var client_id = process.env.CLIENT_ID; // your clientId
 var client_secret = process.env.CLIENT_SECRET; // Your secret
 console.log(client_id)
 console.log(typeof(client_id))
+console.log("Hello")
 var redirect_uri = 'https://spotifytimelinebackend-production.up.railway.app/callback'; 
 
 const generateRandomString = (length) => {
