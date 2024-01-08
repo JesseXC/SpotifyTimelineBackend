e3bf8c942101792f626ef480a2601481ac9fbec7
JesseXC <jcruz2@oberlin.edu>
2024-01-07 22:38:31 -0800

    Fixed typo

--- app.js
+++ app.js
@@ -15,13 +15,12 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 const { type } = require('os');
 
-const PORT = process.env.PORT || 9000;
 var client_id = process.env.CLIENT_ID; // your clientId
 var client_secret = process.env.CLIENT_SECRET; // Your secret
 console.log(client_id)
 console.log(typeof(client_id))
 console.log("Hello")
-var redirect_uri = `localhost:${PORT}/callback`; 
+var redirect_uri = 'spotifytimelinebackend-production.up.railway.app/callback'; 
 
 const generateRandomString = (length) => {
   return crypto
@@ -146,5 +145,6 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
