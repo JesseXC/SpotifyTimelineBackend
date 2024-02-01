682c7631b558b8ce439c5d3ec5b26097b69e0e72
roadgoat19 <kenneth.hoskins@gmail.com>
2024-02-01 15:57:42 -0800

    updated err message

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ app.get('/callback', function(req, res) {
         
         db.query('SELECT * FROM users WHERE spotify_username = ?', [spotifyUsername], (err, result) => {
           if (err) {
-            throw err;
+            console.error("Error accessing database: ", err)
           } else if (result.length === 0) {
             // User does not exist, insert new user
             db.query('INSERT INTO users (spotify_username, song_ids) VALUES (?, ?)', [spotifyUsername, ""], (insertErr) => {
